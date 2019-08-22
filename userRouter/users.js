0e3654c02144ab95424cc8fee2218720dac8a507
the-best-developer <im.dylan.gosnell@gmail.com>
2019-08-21 21:55:54 -0400

    Added login endpoint and log-in checking middleware

--- userRouter/users.js
+++ userRouter/users.js
@@ -3,53 +3,91 @@ const userDb = require('../database/userDb.js');
 const router = express.Router();
 const bcrypt = require('bcryptjs')
 
-router.get('/users', async (req, res) => {
+const isLoggedIn = async (req, res, next) => {
+    const user = req.headers;
+    // Check for username and password
+    if (!user.username || !user.password) {
+        return res.status(500).json({message: "Please log in :)"})
+    }
+    // Select user matching header data
+    const selectUser = await userDb('users').where({ username: user.username }).first();
+    // Check for problems
+    if (!selectUser) {
+        return res.status(500).json({message: "Could not find new user"})
+    };
+    // Check if password of selected user matches header password
+    if (!bcrypt.compareSync(user.password, selectUser.password)) {
+        return res.status(500).json({message: "Incorrect password"})
+    }
+    // If logged in, continue
+    next();
+}
+
+router.get('/users', isLoggedIn, async (req, res) => {
 
     try {
         const users = await userDb('users');
-        res.json(users);
+        return res.json(users);
     }
     catch (err) {
-        res.status(500).json({ message: 'Failed to get users' });
+        return res.status(500).json({ message: 'Failed to get users' });
     }
 });
 
 router.post('/register', async (req, res) => {
     let user = req.body;
-    
-        try{
-            // Check for username and password
-            if (!user.username || !user.password) {
-                res.status(500).json({message: "missing username or password"})
-                return null;
-            }
-            // Hash password
-            user.password = bcrypt.hashSync(user.password, 10);
-            // Add user to database using hashed password
-            const addUser = await userDb('users').insert(user);
-            // Check for problems
-            if (!addUser) {
-                res.status(500).json({message: "Could not add user"})
-                return null;
-            };
-            // Select newly added user and return it
-            const selectUser = await userDb('users').where({id: addUser[0]}).first();
-            // Check for problems
-            if (!selectUser) {
-                res.status(500).json({message: "Could not find new user"})
-                return null;
-            };
-            // Return new user
-            res.status(200).json(selectUser);
-        }
-        catch (err) {
-            res.status(500).json({err: err.message})
-        }
-  });
 
-function isLoggedIn(req, res, next) {
-    
-    next();
-  }
+    try{
+        // Check for username and password
+        if (!user.username || !user.password) {
+            return res.status(500).json({message: "missing username or password"})
+        }
+        // Hash password
+        user.password = bcrypt.hashSync(user.password, 10);
+        // Add user to database using hashed password
+        const addUser = await userDb('users').insert(user);
+        // Check for problems
+        if (!addUser) {
+            return res.status(500).json({message: "Could not add user"})
+        };
+        // Select newly added user and return it
+        const selectUser = await userDb('users').where({id: addUser[0]}).first();
+        // Check for problems
+        if (!selectUser) {
+            return res.status(500).json({message: "Could not find new user"})
+        };
+        // Return new user
+        return res.status(200).json(selectUser);
+    }
+    catch (err) {
+        return res.status(500).json({err: err.message})
+    }
+});
+
+router.post('/login', async (req, res) => {
+    let user = req.body;
+
+    try{
+        // Check for username and password
+        if (!user.username || !user.password) {
+            return res.status(500).json({message: "missing username or password"})
+        }
+        // Select username matching the post data
+        const selectUser = await userDb('users').where({ username: user.username }).first();
+        // Check for problems
+        if (!selectUser) {
+            return res.status(500).json({message: "Could not find new user"})
+        };
+        // Compare saved user hash and post data user hash
+        if (!bcrypt.compareSync(user.password, selectUser.password)) {
+            return res.status(500).json({message: "Incorrect password"})
+        }
+        // Logged in!
+        return res.status(500).json({message: `Logged in as ${selectUser.username}!`})
+    }
+    catch (err) {
+        return res.status(500).json({err: err.message})
+    }
+});
 
 module.exports = router;
